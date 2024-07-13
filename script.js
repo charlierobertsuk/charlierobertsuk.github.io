e25208fba4208b0ba6196a06ef02b7ee8da46639
Charlie Roberts <charlie@robertsclan.co.uk>
2024-07-13 17:22:37 +0100

    dice roll

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const diceDisplay = document.querySelectorAll(`.dice`); // now change dice display
+
+const diceRoll = function () {
+  const roll = Math.trunc(Math.random() * 6) + 1;
+  return roll;
+};
+
+//for (let i = 0; i < )
